52402da4a60fa9a46cbc700a92f7430761e764b9
Hilogen <73134887+Hilogen@users.noreply.github.com>
2021-11-11 22:24:42 +0530

    Add files via upload

--- flask_api.py
+++ flask_api.py
@@ -1,37 +1,34 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from scraper import key_word_meme
 
 app = Flask(__name__)
 
-def fetch(query = None, token = None):
-    output = ""
-    if token == None:
-        output =  f"User searched with '{query}' and did not use a token."
-    elif query == None:
-        output = f"User used the token '{token}' but did not enter any search queries."
+def fetch(query = None):
+    if query == "#random":
+        query = [""]
     else:
-        output =  f"User searched with '{query}' and used token '{token}'"
+        query = query.split("+")
+    print(query)
+    try:
+        data = key_word_meme(query)
+        response = {"response":"Successfully fetched meme.", "meme":data}
     
-    return {"response":output}
+    except:
+        response = {"response":"Error collecting the meme. It might be an error in your request or it might be an error on our side. If you believe it's an error on our side, please visit our documentation page."}
+
+    return response
 
 @app.route("/")
 def home():
-    return 'click <a href = "url">here</a> for documentation.'
+    return(fetch(query = "#random"))
 
 @app.route("/query=<query>/")
 def search(query = None):
     return(fetch(query=query))
 
-@app.route("/<token>/query=<query>/")
-def prem_search(token, query):
-    return(fetch(query = query, token = token))
-
-@app.route("/<token>/")
-def prem_search_noquery(token):
-    return(fetch(token = token))
-
-@app.route("/<token>/<query>")
-def prem_search_wrongquery(token, query):
-    return {"response":f"User used token '{token}' but did not enter the search query properly."}
+@app.route("/help/")
+def help():
+    return redirect("https://www.youtube.com/")
 
 if __name__ == "__main__":
     app.run()
